f44f9b9ebed7d5fca15ff4398f0b5e6060d315cd
afeiship <1290657123@qq.com>
2017-08-05 11:35:36 +0800

    a->div

--- src/cells/cell.js
+++ src/cells/cell.js
@@ -6,7 +6,7 @@ export default class extends Component {
   render(props) {
     const {className, access, link, ...options} = props;
     return (
-      <a href="javascript:void(0);"
+      <div href="javascript:void(0);"
         className={classNames('weui-cell', {
         'weui-cell_access': access,
         'weui-cell_link': link
